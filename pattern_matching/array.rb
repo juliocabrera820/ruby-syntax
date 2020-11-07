049a7378e586ef727bb3a7fd8e888bd92c131f47
julio cabrera <juliocabrera820@gmail.com>
2020-11-07 15:42:18 -0600

    pattern matching examples

---
+++ pattern_matching/array.rb
@@ -0,0 +1,4 @@
+languages = %w[java go ruby elixir javascript]
+languages in [first, *middle, last]
+
+puts middle
---
+++ pattern_matching/hash.rb
@@ -0,0 +1,8 @@
+language = {name: "ruby", version: 2.7}
+language in {name: name, version: v}
+
+puts v
+
+laptop = {brand: "dell", ram: 16}
+laptop in {ram:}
+puts ram

c7820c0c9ff37c7330037e2837dd76e8272cb2b4
julio cabrera <juliocabrera820@gmail.com>
2020-11-08 12:09:46 -0600

    pattern matching in a nested hash

---
+++ pattern_matching/nested_hash.rb
@@ -0,0 +1,13 @@
+developer = {
+    stack: {
+        back: {
+            ror: "Ruby on Rails",
+            langs: ["js","ruby","elixir"]
+        }
+    }
+}
+
+developer in {stack: {back: {ror:, langs: [*, last]}}}
+
+puts ror
+puts last
